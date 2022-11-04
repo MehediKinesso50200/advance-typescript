dd46bf175b6087353d85abfa574b09de4e2596ae
MehediKinesso50200 <mehedi.hasan@kinesso.com>
2022-11-04 21:59:52 +0800

    update

---
+++ src/exercises/01-common-use-case.ts
@@ -0,0 +1,45 @@
+export {};
+
+interface UserResponse {
+  status: string;
+  message: string;
+  data: {
+    id: number;
+    name: string;
+  };
+}
+
+interface BookResponse {
+  status: string;
+  message: string;
+  data: {
+    id: number;
+    title: string;
+    author: string;
+  }[];
+}
+
+interface ApiResponse<T> {
+  status: string;
+  message: string;
+  data: T;
+}
+interface User {
+  id: number;
+  name: string;
+}
+
+interface Book {
+  id: number;
+  title: string;
+  author: string;
+}
+
+// type UserResponse = ApiResponse<User[]>;
+// type BookResponse = ApiResponse<Book[]>;
+
+function display<T extends User>(per: T): void {
+  console.log(`${per.id} ${per.name}`);
+}
+
+display({ji: 'd', id: 1, name: 'name'});
