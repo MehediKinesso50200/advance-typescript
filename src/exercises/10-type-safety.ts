8a93e1d877739c6a013e7ea9935a3dd1a34d4fe7
MehediKinesso50200 <mehedi.hasan@kinesso.com>
2022-11-05 02:26:23 +0800

    1-5 done

--- src/exercises/10-type-safety.ts
+++ src/exercises/10-type-safety.ts
@@ -1,28 +1,24 @@
+export {};
 interface Order {
   total: number;
   currency: string;
-}
-
-interface Stripe {
+  paymentType: 'stripe' | 'paypal';
   card: number;
-}
-interface PayPal {
   email: string;
 }
 
-type StripeOrder = Order & Stripe; // { total: number; currency: string; card: number; }
-type PayPalOrder = Order & PayPal; // { total: number; currency: string; email: string; }
+// const isStripe = (order: StripeOrder | PayPalOrder): order is StripeOrder => {
+//   return 'card' in (order as StripeOrder);
+// };
 
-const isStripe = (order: StripeOrder | PayPalOrder): order is StripeOrder => {
-  return 'card' in (order as StripeOrder);
-};
-
-const processOrder = (order: StripeOrder | PayPalOrder) => {
-  if (isStripe(order)) {
+export function processOrder(order: Order) {
+  if (order.paymentType == 'paypal') {
     order.card;
-    // order.email
+    order.email;
   } else {
     order.email;
-    // order.card
+    order.card;
   }
-};
+
+  return order;
+}
