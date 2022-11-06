e220a9a7f14ee0ceb0d2b2bd95f65b6d13fc4ce7
MehediKinesso50200 <mehedi.hasan@kinesso.com>
2022-11-06 22:31:19 +0800

    update presentation

---
+++ src/exercises/08-generic-class.ts
@@ -0,0 +1,47 @@
+import {Product} from '../exercises-solution/01-common-use-case';
+
+interface Stack<T> {
+  length: number;
+  store: T[];
+
+  pop(): T | undefined;
+  push(item: T): void;
+  peek(item: T): T;
+}
+
+class MyStack<T> implements Stack<T> {
+  store: T[] = [];
+
+  get length() {
+    return this.store.length;
+  }
+
+  pop(): T | undefined {
+    return this.store.pop();
+  }
+
+  push(house: T) {
+    this.store.push(house);
+  }
+
+  peek() {
+    return this.store[this.store.length - 1];
+  }
+}
+
+const myHouseInstance = new MyStack<Product>();
+myHouseInstance.length;
+myHouseInstance.push({
+  id: 10,
+  name: 'T-shirt',
+  description: 'Lorem ipsum dolor sit amet...',
+  images: [],
+  categories: 'cloth',
+  inventory: 20,
+  price: 250,
+  details: {
+    size: 'sm',
+    color: '#fffff'
+  }
+});
+myHouseInstance.peek();
